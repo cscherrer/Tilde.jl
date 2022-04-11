cfad32efe3130c9386c9b9472c4a99119c02bf7f
Chad Scherrer <chad.scherrer@gmail.com>
2022-04-11 09:23:07 -0700

    update insupport

--- src/primitives/insupport.jl
+++ src/primitives/insupport.jl
@@ -1,41 +1,6 @@
-
-
-using NestedTuples: lazymerge
-import MeasureTheory
-
 import MeasureBase: insupport
 export insupport
 
-# function MeasureBase.insupport(c::AbstractConditionalModel{A,B,M}, x=NamedTuple()) where {A,B,M}
-#     _insupport(M, model(c), argvals(c), observations(c), x)
-# end
-
-# export sourceInsupport
-
-# sourceInsupport(m::AbstractModel) = sourceInsupport()(model(m))
-
-# function sourceInsupport()
-#     function(_m::AbstractModel)
-#         proc(_m, st :: Assign)     = :($(st.x) = $(st.rhs))
-#         proc(_m, st :: Return)     = nothing
-#         proc(_m, st :: LineNumber) = nothing
-#         function proc(_m, st :: Sample)
-#             x = st.x
-#             rhs = st.rhs
-#             @q begin
-#                 Tilde.dynamic(Tilde.insupport($rhs, $x)) || return false
-#                 $x = Tilde.predict($rhs, $x)
-#             end
-#         end
-
-#         wrap(kernel) = @q begin
-#             $kernel
-#             return true
-#         end
-
-#         buildSource(_m, proc, wrap) |> MacroTools.flatten
-#     end
-# end
-
-# # MeasureTheory.insupport(d::Distribution, val, tr) = logdensityof(d, val)
-
+@inline function insupport(m::AbstractConditionalModel, x::NamedTuple)
+    mapreduce(insupport, (a,b) -> a && b, measures(m,x), x)
+end
