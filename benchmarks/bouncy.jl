e496ac89628aee51212de68274a6ca84b81718e4
mschauer <moritzschauer@web.de>
2022-06-10 17:54:03 +0200

    Cosmetics

--- benchmarks/bouncy.jl
+++ benchmarks/bouncy.jl
@@ -36,21 +36,24 @@ model = @model (At, y, σ) begin
 end
 
 σ = 100.0
-
 post = model(At, y, σ) | (;y)
 
-#ℓ(θ) = logdensityof(post, (;θ))
-ℓ(θ) = logdensityof(post, transform(as(post), θ))
-obj(θ) = -ℓ(θ)
+let post = post, σ = σ
+    global ℓ, obj, dneglogp, ∇neglogp!
+    as_post = as(post)
+    ℓ(θ) = logdensityof(post, transform(as_post, θ))
+    obj(θ) = -ℓ(θ)
 
-function dneglogp(t, x, v, args...) # two directional derivatives
-    u = ForwardDiff.derivative(t -> obj(x + t*v), Dual{:hSrkahPmmC}(0.0, 1.0))
-    u.value, u.partials[]
-end
+    function dneglogp(t, x, v, args...) # two directional derivatives
+        u = ForwardDiff.derivative(t -> obj(x + t*v), Dual{:hSrkahPmmC}(0.0, 1.0))
+        u.value, u.partials[]
+    end
 
-function ∇neglogp!(y, t, x, args...)
-    ForwardDiff.gradient!(y, obj, x)
-    y
+    function ∇neglogp!(y, t, x, args...)
+        ForwardDiff.gradient!(y, obj, x)
+        y
+    end
+    ℓ, obj, dneglogp, ∇neglogp!
 end
 
 # Try things out
@@ -61,10 +64,9 @@ end
 d = 25 # number of parameters 
 t0 = 0.0
 x0 = zeros(d) # starting point sampler
-# estimated posterior mean
-xhat = [3.412, -0.5916, 0.03527, -0.3873, 0.004466, -0.2345, -0.15, -0.2164, 0.01229, 0.1739, -0.009764, -0.3217, 0.2173, 0.08189, -0.2851, -1.59, 0.6661, -1.003, 1.078, 1.401, 0.3271, -0.1361, -0.6437, -0.06795, -0.05282]
-
-T = 5000. # end time (similar to number of samples in MCMC)
+# estimated posterior mean (n=100000, 797s)
+μ̂ = [3.406, -0.5918, 0.0352, -0.3874, 0.004481, -0.2346, -0.1495, -0.2184, 0.01219, 0.1731, -0.00976, -0.3224, 0.2168, 0.08002, -0.2829, -1.581, 0.6666, -0.9984, 1.081, 1.405, 0.327, -0.1357, -0.6446, -0.06583, -0.04994]
+n = 1000
 c = 0.01 # initial guess for the bound
 using Pathfinder
 init_scale=1
@@ -76,7 +78,7 @@ MAP = result.optim_solution # MAP, could be useful for control variates
 
 # define BouncyParticle sampler (has two relevant parameters) 
 Z = BouncyParticle(∅, ∅, # ignored
-    2.0, # momentum refreshment rate 
+    1.0, # momentum refreshment rate 
     0.95, # momentum correlation / only gradually change momentum in refreshment/momentum update
     0.0, # ignored
     M # cholesky of momentum precision
@@ -106,11 +108,17 @@ function collect_sampler(t, sampler, n)
     tv
 end
 elapsed_time  = @elapsed begin
-    tv = collect_sampler(as(post), sampler, 1000)
+    tv = collect_sampler(as(post), sampler, n)
 end
+@show elapsed_time
 
 using MCMCChains
 bps_chain = MCMCChains.Chains(tv.θ)
 bps_chain = setinfo(bps_chain,  (;start_time=0.0, stop_time = elapsed_time))
+
+print("μ̂ = ", round.(mean(bps_chain).nt[:mean], sigdigits=4))
+
 bps_chain
+
+
 
