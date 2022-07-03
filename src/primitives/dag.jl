5ccda6310a52773fc477bc6621f419a3be3b73c5
Chad Scherrer <chad.scherrer@gmail.com>
2022-07-03 14:19:39 -0700

    dag

--- src/primitives/dag.jl
+++ src/primitives/dag.jl
@@ -15,14 +15,14 @@ context_meta(ctx::AbstractContext) = ctx.meta
 
 
 
+export dag
 
 
 
 
 
-
-
-
+using Graphs
+using MetaGraphsNext
 
 
 
@@ -31,39 +31,40 @@ struct MarkovContext{T,M} <: AbstractContext
     meta::M
 end
 
+function markovinate(nt::NamedTuple{N,T}) where {N,T}
+    vals = tuple((MarkovContext(v, Set((k,identity))) for (k,v) in pairs(nt))...)
+    NamedTuple{N}(vals)
+end
+
 MarkovContext(x::MarkovContext) = x
 MarkovContext(x) = MarkovContext(x, Set())
 
 markov_parents(x) = Set()
 
 markov_value(x::MarkovContext) = x.value
-markov_parents(x::MarkovContext) = x.parents
+markov_parents(x::MarkovContext) = x.meta
+
+function dag(m::AbstractConditionalModel, pars)
+    cfg = NamedTuple()
+    pars = markovinate(pars)
+    dag = MetaGraph(DiGraph(), Label = Tuple{Symbol, Any})
+    ctx =  (dag = dag,)
+    gg_call(dag, m, pars, cfg, ctx, (r, ctx) -> ctx)
+end
 
 # When a Tilde primitive `f` is called, every `g(args...)` is converted to
 # `call(f, g, args...)` 
-function call(::typeof(dag), g, args...)
+function call(::typeof(dag), g, args...; kwargs...)
     val = g(map(markov_value, args)...; map(markov_value, kwargs)...)
     parents = union(map(markov_parents, args)..., map(ctx_meta, kwargs)...)
     MarkovContext(val, parents)
 end
 
-function dag(m::AbstractConditionalModel, pars)
-    ctx =  SimpleDigraph()
-    gg_call(dag, m, pars, cfg, ctx, (r, ctx) -> ctx)
-end
-
-@inline function tilde(::typeof(dag), lens, xname, x, d, pars, ctx::MetaDiGraph)
-    xnew = set(x.value, Lens!!(lens), rand(cfg.rng, d))
-    ctx′ = merge(ctx, NamedTuple{(dynamic(xname),)}((xnew,)))
-    (xnew, ctx′, nothing)
-end
-
-
-@inline function tilde(::typeof(unsafe_logdensityof), lens, xname, x, d, cfg, ctx::NamedTuple)
-    x = x.value
+@inline function tilde(::typeof(dag), x::MaybeObserved{X}, lens, d, pars, ctx) where {X}
     for p in markov_parents(d)
         # Add a new edge in the DAG
-        ctx.dag[(dynamic(xname), lens), p] = nothing
+        ctx.dag[p, Set(((X, lens),))] = nothing
     end
-    (MarkovContext(x), ctx, ctx.dag)
+    (MarkovContext(x, (X, lens)), ctx, ctx.dag)
 end
+
