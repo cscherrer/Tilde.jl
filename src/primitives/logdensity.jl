802aae4ca019e19ef0368e532a7261a9feabd3e6
Chad Scherrer <chad.scherrer@gmail.com>
2022-08-10 12:26:11 -0700

    make logdensity_def work

--- src/primitives/logdensity.jl
+++ src/primitives/logdensity.jl
@@ -11,7 +11,7 @@ end
 
 @inline retfun(cfg::LogdensityConfig{typeof(logdensityof)}, r, ctx) = ctx.ℓ
 @inline retfun(cfg::LogdensityConfig{typeof(unsafe_logdensityof)}, r, ctx) = ctx.ℓ
-
+@inline retfun(cfg::LogdensityConfig{typeof(logdensity_def)}, r, ctx) = ctx.ℓ
 
 @inline function MeasureBase.logdensityof(
     cm::AbstractConditionalModel{M,A,O},
@@ -55,5 +55,28 @@ end
     # insupport(d, lens(x)) || return (x, ReturnNow(-Inf))
     pred = predict(d, lens(x))
     @reset ctx.ℓ += unsafe_logdensityof(d, lens(x))
+    (pred, ctx)
+end
+
+
+@inline function MeasureBase.logdensity_def(
+    cm::AbstractConditionalModel{M,A,O},
+    pars::NamedTuple;
+) where {M,A,O}
+    cfg = LogdensityConfig(logdensity_def)
+    runmodel(cfg, cm, pars, (ℓ = 0.0,))
+end
+
+@inline function tilde(
+    cfg::LogdensityConfig{typeof(logdensity_def)},
+    x::MaybeObserved{X},
+    lens,
+    d,
+    ctx::NamedTuple,
+) where {X}
+    x = value(x)
+    # insupport(d, lens(x)) || return (x, ReturnNow(-Inf))
+    pred = predict(d, lens(x))
+    @reset ctx.ℓ += logdensity_def(d, lens(x))
     (pred, ctx)
 end
